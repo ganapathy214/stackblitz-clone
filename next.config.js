150de0c50b297d2f8d970da95ddc3bfe1b877488
ganapathy <ganapathy214@gmail.com>
2023-07-06 14:07:20 +0530

    Webpack ignore case

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["antd", "rc-util", "@ant-design","rc-pagination","rc-picker"],
+  webpack: (config) => {
+    // Ignore case in the imports of folders inside node_modules
+    config.plugins.push(new webpack.IgnorePlugin(/[/\\]node_modules[/\\]/, /[/\\]node_modules[/\\](.*[/\\])?/i));
+
+    return config;
+  },
 }
 
 // module.exports = nextConfig
