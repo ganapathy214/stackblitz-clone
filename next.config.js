731c2e6a159c575b495044def707817dc265e616
ganapathy <ganapathy214@gmail.com>
2023-07-06 13:27:39 +0530

    HF: Config update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["antd", "rc-util","@ant-design","/rc-(.*)/",""],
+  transpilePackages: ["antd", "rc-util", "@ant-design","rc-pagination","rc-picker"],
 }
 
 // module.exports = nextConfig
