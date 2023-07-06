f17a4c1471f311407261cb2442a1208287914749
ganapathy <ganapathy214@gmail.com>
2023-07-06 14:34:56 +0530

    Removed webpack

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
-const webpack = require('webpack');
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["antd", "rc-util", "@ant-design","rc-pagination","rc-picker"],
-  webpack: (config) => {
-    // Ignore case in the imports of folders inside node_modules
-    config.plugins.push(new webpack.IgnorePlugin(/[/\\]node_modules[/\\]/, /[/\\]node_modules[/\\](.*[/\\])?/i));
-
-    return config;
-  },
 }
 
 // module.exports = nextConfig
